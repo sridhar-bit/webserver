1cff40d85fade4411d52421a47b1119bb806c92c
root <root@ip-172-31-81-113.ec2.internal>
2021-10-11 03:10:20 +0000

    updated script

--- webserver.sh
+++ webserver.sh
@@ -5,12 +5,12 @@ service httpd start
 touch /var/www/html/index.html
 chmod 777 /var/www/html/index.html
 echo "<h1>this webserver via script</h1>" > /var/www/html/index.html 
-
-
 elif [[ `which apt` ]]; then
 apt install apache2 -y
 rm -rf /var/www/html/index.html
 touch /var/www/html/index.html
 chmod 777 /var/www/html/index.html
-echo "<h1>this webserver on ubuntu via script</h1>" > /var/www/html/index.html 
-
+echo "<h1>this webserver on ubuntu via script</h1>" >> /var/www/html/index.html 
+else
+echo "the server is unkonw"
+fi
